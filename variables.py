33c350fa6db4bae6762d2275b6591c76e3e9af20
Asan Emirsaleh <asantaraqtasli@gmail.com>
2020-07-15 00:40:11 +0300

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,7 +1,4 @@
 ## List of variables used in script
 
-fastq_fastmode = "C:/rough/ht_good/fast_mode/sequencing_summary_combined.txt"
-fastq_hacmode = "C:/rough/ht_good/hac_mode/sequencing_summary_combined.txt"
-
-# fastq_fastmode = "C:/rough/ht_good/fast_mode/test2.txt"
-# fastq_hacmode = "C:/rough/ht_good/hac_mode/test.txt"
+fastq_fastmode = "C:/example_path/fast_mode/sequencing_summary_combined.txt"
+fastq_hacmode = "C:/example_path/hac_mode/sequencing_summary_combined.txt"
